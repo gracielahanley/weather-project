e0827a9a054152421a2a241fa55a4d141aada09e
graciela <gracielahanley@gmail.com>
2022-11-27 00:12:39 -0400

    units changed, api changed, icon added

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
-const apiKey = `597c40c39084687093b091cd48b366f8`;
+const apiKey = `463fd7t5fb4b4f05a6o78db7b9aa0a0a`;
 
 function getCity() {
     const value = document.querySelector("#inputCity").value;
     const city = value.trim().toLowerCase()
-    console.log(city);
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    console.log(apiUrl);
+    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     axios.get(`${apiUrl}`).then(weather);
 }
 
-function thisDay(){
-let now = new Date();
-let hours = now.getHours();
-let minutes = now.getMinutes();
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "thursday", "Friday", "Saturday"];
-let day = days[now.getDay()];
-let result = `${day} ${hours}: ${minutes}`;
-let dateHour = document.querySelector(".day");
-dateHour.innerHTML = result
+function thisDay() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "thursday", "Friday", "Saturday"];
+    const day = days[now.getDay()];
+    const result = `${day} ${hours}: ${minutes}`;
+    const dateHour = document.querySelector(".day");
+    dateHour.innerHTML = result
 };
 thisDay();
 
+function frontChanges(value, id) {
+    const doc = document.querySelector(id);
+    doc.innerHTML = value;
+}
+
+let temp = null;
+
 function weather(response) {
-    // let temp = Math.round(response.data.main.temp);
-    // let numberTemp = document.querySelector("#numberTemp");
-    // numberTemp.innerHTML = temp;
-    const temp = Math.round(response.data.main.temp);
-    frontChanges(temp,'#numberTemp')
-    const cityName = response.data.name;
+    temp = Math.round(response.data.temperature.current);
+    frontChanges(temp, '#numberTemp')
+    const cityName = response.data.city;
     frontChanges(cityName, '#title-city');
-    const sky = response.data.weather[0].main;
+    const sky = response.data.condition.description;
     frontChanges(sky, ".type");
-    const humidity = response.data.main.humidity;
+    const humidity = response.data.temperature.humidity;
     frontChanges(`Humidity: ${humidity}%`, "#humidity");
     const wind = response.data.wind.speed;
     frontChanges(`Wind: ${wind}km/h`, "#wind");
-    console.log(response)
-}
-
-function frontChanges(value,id){
-    const doc = document.querySelector(id);
-    doc.innerHTML = value;
+    document.getElementById('icon').src = response.data.condition.icon_url;
 }
 
 const button = document.querySelector(".btn");
@@ -52,23 +49,40 @@ input.addEventListener("submit", getCity);
 function success(pos) {
     const lat = pos.coords.latitude;
     const lon = pos.coords.longitude;
-    console.log(lat)
-    console.log(lon)
     currentCity(lat, lon)
 }
-function positionData(){
+function positionData() {
     const position = navigator.geolocation.getCurrentPosition(success);
 }
 function currentCity(lat, lon) {
     const city = document.querySelector("#inputCity").value;
-    console.log(city);
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-    console.log(apiUrl);
+    const apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${apiKey}&units=metric`;
     axios.get(`${apiUrl}`).then(weather);
 }
 
 const current = document.querySelector("#current");
 current.addEventListener("click", positionData);
 
+function showFahrenheit(event) {
+    event.preventDefault();
+    celsiusElement.classList.remove("active");
+    fahrenheitElement.classList.add("active");
+    const fahrenheitTemperature = Math.round((temp * 1.8) + 32);
+    frontChanges(fahrenheitTemperature, '#numberTemp')
+}
 
+const fahrenheitElement = document.querySelector("#fahrenheit");
+fahrenheitElement.addEventListener("click", showFahrenheit);
 
+function showCelsius(event) {
+    event.preventDefault();
+    fahrenheitElement.classList.remove("active");
+    celsiusElement.classList.add("active");
+    const celsiusTemperature = temp;
+    frontChanges(temp, '#numberTemp');
+}
+
+const celsiusElement = document.querySelector("#celsius");
+celsiusElement.addEventListener("click", showCelsius);
+
+positionData();
